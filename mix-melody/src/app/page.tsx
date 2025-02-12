76f6aec3020401474dc5aec1eb1c60f2db39c132
DK <ashukr8081@gmail.com>
2025-02-13 02:56:13 +0530

    Update Home page layout and refine Navbar component structure

--- mix-melody/src/app/page.tsx
+++ mix-melody/src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
 <>
+<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+<h1 className="text-center">Hello</h1>
+</main>
 </>
   );
 }
--- mix-melody/src/components/Navbar.tsx
+++ mix-melody/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 
 function Navbar({ className }: { className?: string }) {
@@ -10,21 +10,25 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-
-        <MenuItem setActive={setActive} active={active} item="Home">
-        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4 text-sm">
+          <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/all-courses">All Courses</HoveredLink>
-            <HoveredLink href="/basic-music-theory">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/basic-music-theory">
+              Basic Music Theory
+            </HoveredLink>
             <HoveredLink href="/composition">Advanced Composition</HoveredLink>
             <HoveredLink href="/song-writing">Song Writing</HoveredLink>
             <HoveredLink href="/music-production">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Contact">
-        </MenuItem>
+        
+        <MenuItem
+          setActive={setActive}
+          active={active}
+          item="Contact"
+        ></MenuItem>
       </Menu>
     </div>
   );
