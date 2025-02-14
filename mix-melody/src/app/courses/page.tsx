6ef16b1b4fc31d7ccdabd68295826eed523d6cea
DK <ashukr8081@gmail.com>
2025-02-14 23:34:11 +0530

    Refactor course and contact pages for improved layout and styling; update course title in JSON

--- mix-melody/src/app/courses/page.tsx
+++ mix-melody/src/app/courses/page.tsx
@@ -6,15 +6,15 @@ import courseData from "@/data/music_courses.json";
 
 function page() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="bg-black">
       <h1 className="text-lg py-12 pt-36 md:text-5xl text-center font-sans font-bold text-white">
         All courses
         {/* ({courseData.courses.length}) */}
       </h1>
       <div className="flex flex-wrap justify-center">
         {courseData.courses.map((course, index) => (
-          <CardContainer key={index} className="inter-var m-4">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardContainer key={index} className="inter-var m-5">
+            <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-3xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto sm:h-[25rem] rounded-xl p-6 border">
               <CardItem
                 translateZ="50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
@@ -33,22 +33,22 @@ function page() {
                   src={course.image}
                   height="800"
                   width="800"
-                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  className="h-50 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                   alt={course.title}
                 />
               </CardItem>
-              <div className="flex justify-between items-center mt-20">
+              <div className="flex justify-between items-center mt-10">
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  className="px-4 py-1 rounded-xl text-xs font-normal dark:text-white"
                 >
                   Try now →
                 </CardItem>
                 <CardItem
                   translateZ={20}
                   as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  className="px-4 py-1 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                 >
                   Sign up
                 </CardItem>
